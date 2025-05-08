4f54df6e510d7dd1b315e0f2b0e12cfb879403d4
Nihar Pipaliya <114608191+niharpipaliya10@users.noreply.github.com>
2025-05-08 09:26:46 +0530

    Add files via upload

---
+++ 1.Class-examples/11.c
@@ -0,0 +1,25 @@
+//wap to delete on specific charactore from given string.
+#include<stdio.h>
+void main(){
+	int i,j;
+	char s[20],ch;
+	printf("Enter a string and ch:\n");
+	scanf("%s %c",s,&ch);
+	printf("before :%s\n",s);
+	for(i=0;s[i];i++)
+	{
+
+		if(s[i]==ch)
+		{  
+			for(j=i;s[j];j++)
+			{
+				s[j]=s[j+1];
+                   
+                                 i--;
+                        }
+		}
+	}
+
+	printf("After :%s\n",s);
+
+}
